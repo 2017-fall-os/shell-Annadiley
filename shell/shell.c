d98830fa90ed2f54234ad79b281ff4c6cbb1bb18
Annadiley Gallegos <annadileyg@msn.com>
2017-09-24 16:35:30 -0600

    Shell works properly, added documentation

--- shell/shell.c
+++ shell/shell.c
@@ -4,8 +4,14 @@
 #include "mytoc.h"
 #include "strcopy.h"
 #include <sys/stat.h>
+#include <sys/wait.h>
+//function declarations
+int isExecutable(char *path);
+void createChildProcess(char * path , char **tokenVect, char **eenvp);
+   
 int equalsExit,pid,execV;
 char stri[1000];
+char *toExecute;
 int main (int atgc, char **arg, char **envp){
   char *enpath;
   for(int i=0;envp[i] != (char *)0;i++){ //as long as the envp contains no 0
@@ -19,6 +25,7 @@ int main (int atgc, char **arg, char **envp){
   
   
   while(equalsExit!=1){
+    toExecute=NULL;
     write(1,"$0",1);                      //display the $ promt
     read(1,stri,sizeof( stri));           //get user input
     stri[sizeof(stri)+1]='\0';
@@ -26,23 +33,52 @@ int main (int atgc, char **arg, char **envp){
     equalsExit =checkEx(stri);            //check if there must be an exit of the loop
 
     if(tokeenVect[0]){                    //if vector is not empty start command
-      
-
-
-
-
-
-      /*   pid = fork();                       //create process to start command
-      if(pid==0){
-	execV=execve(tokeenVect[0],tokeenVect,envp);
+      if(isExecutable(tokeenVect[0])){    //If it is a full path
+	printf("Excecuting command:\n", tokeenVect[0]);
+	createChildProcess(tokeenVect[0] ,tokeenVect, envp); //create process
+	}
+      else{                               //search for full path
+	for( int i=0;pathVect[i] !=(char*)0;i++){
+	  char *temp = strcat(pathVect[i],"/");
+	  char *fullPath =strcat(temp,tokeenVect[0]);
+	  if(isExecutable(fullPath)){
+	      createChildProcess(fullPath ,tokeenVect, envp); //create process
+	      break;
+	    
+	  }
+	}
+	printf("Command not found\n");
       }
-      else if(
-
-      
-      }*/
-    }
-     for(int i =0; i<sizeof(stri);i++){     //free previous space
+   
+    for(int i =0; i<sizeof(stri);i++){     //free previous space
       stri[i]='\0';
-    }                   
-  } return 0;
+    }                 
+    return 0;
+    }
+  }
 }
+int isExecutable(char *path){            //Method used to check if hte given path to a file can be actually executed or not
+  struct stat s;
+  return (((stat(path,&s)==0)&&(s.st_mode & S_IXOTH))?(short)1:(short)0); //checks if there is a path and if we have execure permission for it.	
+}
+ void createChildProcess(char * path , char **tokenVect, char **eenvp){
+   int exitNorm;                          //used to check if child process terminated normaly
+   pid = fork();                          //create child process
+   if(pid == 0){                          
+     execve(path,tokenVect,eenvp);        //execute child process
+     
+   }
+   else if (pid <0){
+     wait(&exitNorm);
+     if(WIFSIGNALED(exitNorm)){
+       printf("Program Terminated with exit code : %d\n",WTERMSIG(exitNorm));
+     }
+
+   }
+   else{
+     printf("Command Not Found\n");
+   }
+
+ }
+ 
+
